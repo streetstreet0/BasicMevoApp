3a40620402f933af37a00f34e07776470fd04c48
streetstreet0 <streetstreet.kjewiss@gmail.com>
2024-02-26 15:22:10 +1300

    decided to change direction, working on the api first

--- app/src/main/java/com/example/basicmivoapp/MainActivity.java
+++ app/src/main/java/com/example/basicmivoapp/MainActivity.java
@@ -2,10 +2,16 @@ package com.example.basicmivoapp;
 
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.core.view.WindowCompat;
@@ -18,14 +24,41 @@ import com.example.basicmivoapp.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    private TextView text;
+    private String url = "https://dev.api.mevo.co.nz";
+    private String parkingEndpoint = "/parking/wellington";
+    private String vehicleEndpoint = "vehicles/wellington";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.testData);
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    String datetime = response.getString("datetime");
+                    text.setText(datetime);
+                }
+                catch (Exception exception) {
+                    Log.d("TEST", "failed to get a datetime");
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("ERROR", "An error occured");
+            }
+        });
+        Volley.newRequestQueue(this).add(request);
     }
 }
