feb74838692812882b072b7f68cfc122c32af4e0
streetstreet0 <streetstreet.kjewiss@gmail.com>
2024-02-27 19:59:54 +1300

    the app now works, just need to clean up the code

--- app/src/main/java/com/example/basicmivoapp/MainActivity.java
+++ app/src/main/java/com/example/basicmivoapp/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.basicmivoapp;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.content.Context;
@@ -11,7 +17,6 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.android.gms.maps.model.Polygon;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -43,11 +48,18 @@ import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Polygon;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.OverlayItem;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -63,8 +75,16 @@ public class MainActivity extends AppCompatActivity {
     private MapView map;
     private final double defaultLongitude = 174.77557;
     private final double defaultLatitude = -41.28664;
-    private final double defaultZoom = 12.5;
+    private final double defaultZoom = 13.5;
+//    private ArrayList<OverlayItem> overlayItems = new ArrayList<OverlayItem>();
 
+    /**
+     * The method starts the application, creates the map, and adds all of the icons onto the map.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -79,6 +99,12 @@ public class MainActivity extends AppCompatActivity {
 
         requestPermissionsIfNecessary(new String[] {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE});
 
+//        Marker cityMarker = new Marker(map);
+//        GeoPoint cityPosition = new GeoPoint(defaultLatitude, defaultLongitude);
+//        cityMarker.setPosition(cityPosition);
+//        cityMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+//        map.getOverlays().add(cityMarker);
+//        cityMarker.setTitle("Wellington");
 
         map.setMultiTouchControls(true);
         IMapController mapController = map.getController();
@@ -87,14 +113,17 @@ public class MainActivity extends AppCompatActivity {
         mapController.setCenter(startPoint);
 
 //        testList = (ListView) findViewById(R.id.testList);
-//        requestData();
+        requestData();
     }
 
+    /**
+     * This method restarts the app if
+     */
     @Override
     protected void onRestart() {
         super.onRestart();
 
-//        requestData();
+//        requestDataTest();
     }
 
     @Override
@@ -103,7 +132,7 @@ public class MainActivity extends AppCompatActivity {
 
         map.onResume();
 
-//        requestData();
+//        requestDataTest();
     }
 
     @Override
@@ -114,6 +143,126 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void requestData() {
+        JsonObjectRequest vehicleRequest = new JsonObjectRequest(Request.Method.GET, baseUrl + vehicleEndpoint, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray features = response.getJSONObject("data").getJSONArray("features");
+                    Log.d("TEST", features.toString());
+
+                    generateVehicleMarkers(features);
+                }
+                catch (Exception exception) {
+                    Log.d("TEST", response.toString());
+                    Log.d("ERROR", "failed to parse vehicle data");
+                    exception.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("ERROR", "An error occured");
+                error.printStackTrace();
+            }
+        });
+
+        JsonObjectRequest parkingRequest = new JsonObjectRequest(Request.Method.GET, baseUrl + parkingEndpoint, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray polygons = response.getJSONObject("data").getJSONObject("geometry").getJSONArray("coordinates");
+                    JSONObject properties = response.getJSONObject("data").getJSONObject("properties");
+                    Log.d("TEST", polygons.toString());
+                    Log.d("TEST", properties.toString());
+
+                    generateParkingPolygons(polygons, properties);
+                }
+                catch (Exception exception) {
+                    Log.d("TEST", response.toString());
+                    Log.d("ERROR", "failed to parse parking data");
+                    exception.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("ERROR", "An error occured");
+                error.printStackTrace();
+            }
+        });
+
+        Volley.newRequestQueue(this).add(vehicleRequest);
+        Volley.newRequestQueue(this).add(parkingRequest);
+    }
+
+    /**
+     * Generates the vehicle markers on the map from the given vehicle data.
+     *
+     * @param features JSONArray of features given by the Mevo API. Each feature contains an icon url and coordinates.
+     * @throws JSONException
+     */
+    private void generateVehicleMarkers(JSONArray features) throws JSONException {
+        for (int featureNum = 0; featureNum < features.length(); featureNum++) {
+            JSONObject vehicle = features.getJSONObject(featureNum);
+            JSONArray vehicleCoordinates = vehicle.getJSONObject("geometry").getJSONArray("coordinates");
+            double longitude = vehicleCoordinates.getDouble(0);
+            double latitude = vehicleCoordinates.getDouble(1);
+
+            GeoPoint vehicleLocation = new GeoPoint(latitude, longitude);
+            String vehicleImageUrl = vehicle.getJSONObject("properties").getString("iconUrl");
+
+
+            Marker vehicleMarker = new Marker(map);
+            vehicleMarker.setPosition(vehicleLocation);
+            vehicleMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+            map.getOverlays().add(vehicleMarker);
+            vehicleMarker.setTitle(vehicleImageUrl);
+        }
+    }
+
+    /**
+     * Generates the parking polygons on the map from the given polygon data.
+     *
+     * @param polygons JSONArray of polygons given by the Mevo API. Each polygon is a list of coordinates
+     * @throws JSONException
+     */
+    private void generateParkingPolygons(JSONArray polygons, JSONObject properties) throws  JSONException {
+        for (int polygonNum = 0; polygonNum < polygons.length(); polygonNum++) {
+            JSONArray JSONpolygon = polygons.getJSONArray(polygonNum);
+            ArrayList<GeoPoint> polygonGeoPoints = new ArrayList<GeoPoint>();
+
+            for (int coordinateNum = 0; coordinateNum < JSONpolygon.length(); coordinateNum++) {
+                JSONArray JSONCoordinate = JSONpolygon.getJSONArray(coordinateNum);
+                double longitude = JSONCoordinate.getDouble(0);
+                double latitude = JSONCoordinate.getDouble(1);
+
+                polygonGeoPoints.add(new GeoPoint(latitude, longitude));
+            }
+            // polygon needs to be closed
+            if (polygonGeoPoints.get(0) != polygonGeoPoints.get(polygonGeoPoints.size()-1)) {
+                polygonGeoPoints.add(polygonGeoPoints.get(0));
+            }
+
+            Polygon polygon = new Polygon();
+            Paint polygonPaint = polygon.getFillPaint();
+            polygonPaint.setColor(Color.parseColor(properties.getString("fill")));
+            polygonPaint.setAlpha((int) (255 * properties.getDouble("fill-opacity")));
+            polygonPaint.setStrokeWidth((float) properties.getDouble("stroke-width"));
+            polygon.setStrokeColor(Color.parseColor(properties.getString("stroke")));
+
+            polygon.setPoints(polygonGeoPoints);
+
+            Log.d("TEST", "generated polygon " + polygonNum + " with starting location: latitude = " + polygonGeoPoints.get(0).getLatitude() + ", longitude = " + polygonGeoPoints.get(0).getLongitude());
+            map.getOverlayManager().add(polygon);
+        }
+    }
+
+    /**
+     * Requests vehicle and parking data from the Mevo API's, then displays them as a list.
+     *
+     * Only works if the testList object is uncommented in activity_main.xml
+     */
+    private void requestDataTest() {
         // clear any old data before retrieving new data
         dataList = new ArrayList<String>();
         JsonObjectRequest vehicleRequest = new JsonObjectRequest(Request.Method.GET, baseUrl + vehicleEndpoint, null, new Response.Listener<JSONObject>() {
